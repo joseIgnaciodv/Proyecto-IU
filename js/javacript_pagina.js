5b12a41f5d99cbc6f187e3f70be1461fbc998ec1
joseIgnaciodv <34340750+joseIgnaciodv@users.noreply.github.com>
2021-06-07 18:23:22 +0200

    Comparar Vehiculos

--- js/javacript_pagina.js
+++ js/javacript_pagina.js
@@ -149,80 +149,80 @@ function guardarFavoritos(){
    var json = listaFavoritos()
    var header = document.getElementsByTagName("header")
      for(i; i < json.length; i++){
-            var tarjeta = document.createElement("div")
-            tarjeta.setAttribute("class", "tarjeta")
-            var espacioIm = document.createElement("div")
-            espacioIm.setAttribute("class", "espacio-imagen")
-            var imagen = document.createElement("img")
-            imagen.src = json[i].imagen
+         var tarjeta = document.createElement("div")
+         tarjeta.setAttribute("class", "tarjeta")
+         var espacioIm = document.createElement("div")
+         espacioIm.setAttribute("class", "espacio-imagen")
+         var imagen = document.createElement("img")
+         imagen.src = json[i].imagen
 
-            var contenedor = document.createElement("div")
-            contenedor.setAttribute("class", "contenedor")
-            var primeraFila = document.createElement("div")
-            primeraFila.setAttribute("class", "primera-fila")
-            var segundaFila = document.createElement("div")
-            segundaFila.setAttribute("class", "segunda-fila")
-            
-            var precioCoche = document.createElement("div")
-            precioCoche.setAttribute("class", "precio-coche")
-            var tituloPrecio = document.createElement("span")
-            tituloPrecio.textContent = "Precio: "
-            tituloPrecio.setAttribute("class", "titulo-precio")
-            var precio = document.createElement("span")
-            precio.setAttribute("class", "precio")
-            precio.textContent = json[i].precio
+         var contenedor = document.createElement("div")
+         contenedor.setAttribute("class", "contenedor")
+         var primeraFila = document.createElement("div")
+         primeraFila.setAttribute("class", "primera-fila")
+         var segundaFila = document.createElement("div")
+         segundaFila.setAttribute("class", "segunda-fila")
+         
+         var precioCoche = document.createElement("div")
+         precioCoche.setAttribute("class", "precio-coche")
+         var tituloPrecio = document.createElement("span")
+         tituloPrecio.textContent = "Precio: "
+         tituloPrecio.setAttribute("class", "titulo-precio")
+         var precio = document.createElement("span")
+         precio.setAttribute("class", "precio")
+         precio.textContent = json[i].precio
 
-            var nombre = document.createElement("span")
-            nombre.setAttribute("class", "marca")
-            nombre.textContent = json[i].marca + " " + json[i].modelo
-            var fav = document.createElement("img")
-            fav.src = "imagenes/estrella_roja.png"
-            fav.setAttribute("class", "favorito")
+         var nombre = document.createElement("span")
+         nombre.setAttribute("class", "marca")
+         nombre.textContent = json[i].marca + " " + json[i].modelo
+         var fav = document.createElement("img")
+         fav.src = "imagenes/estrella_roja.png"
+         fav.setAttribute("class", "favorito")
 
-            var anio = document.createElement("span")
-            anio.setAttribute("class", "año")
-            anio.textContent = json[i].año
-            var km = document.createElement("span")
-            km.setAttribute("class", "km")
-            km.textContent = json[i].km
-            var combustible = document.createElement("span")
-            combustible.setAttribute("class", "combustible")
-            combustible.textContent = json[i].combustible
-            var localidad = document.createElement("span")
-            localidad.setAttribute("class", "localidad")
-            localidad.textContent = json[i].localidad
+         var anio = document.createElement("span")
+         anio.setAttribute("class", "año")
+         anio.textContent = json[i].año
+         var km = document.createElement("span")
+         km.setAttribute("class", "km")
+         km.textContent = json[i].km
+         var combustible = document.createElement("span")
+         combustible.setAttribute("class", "combustible")
+         combustible.textContent = json[i].combustible
+         var localidad = document.createElement("span")
+         localidad.setAttribute("class", "localidad")
+         localidad.textContent = json[i].localidad
 
-            var datos = document.createElement("div")
-            datos.setAttribute("class", "datos-coche")
+         var datos = document.createElement("div")
+         datos.setAttribute("class", "datos-coche")
 
-            var enlace = document.createElement("a")
-            enlace.href = "descripcion_vehiculo.html"
+         var enlace = document.createElement("a")
+         enlace.href = "descripcion_vehiculo.html"
 
-            espacioIm.appendChild(imagen)
+         espacioIm.appendChild(imagen)
 
-            precioCoche.appendChild(tituloPrecio)
-            precioCoche.appendChild(precio)
+         precioCoche.appendChild(tituloPrecio)
+         precioCoche.appendChild(precio)
 
-            primeraFila.appendChild(precioCoche)
-            primeraFila.appendChild(nombre)
-            primeraFila.appendChild(fav)
+         primeraFila.appendChild(precioCoche)
+         primeraFila.appendChild(nombre)
+         primeraFila.appendChild(fav)
 
-            datos.appendChild(anio)
-            datos.appendChild(km)
-            datos.appendChild(combustible)
-            datos.appendChild(localidad)
+         datos.appendChild(anio)
+         datos.appendChild(km)
+         datos.appendChild(combustible)
+         datos.appendChild(localidad)
 
-            segundaFila.appendChild(datos)
+         segundaFila.appendChild(datos)
 
-            contenedor.appendChild(primeraFila)
-            contenedor.appendChild(segundaFila)
+         contenedor.appendChild(primeraFila)
+         contenedor.appendChild(segundaFila)
 
-            tarjeta.appendChild(espacioIm)
-            tarjeta.appendChild(contenedor)
+         tarjeta.appendChild(espacioIm)
+         tarjeta.appendChild(contenedor)
 
-            enlace.appendChild(tarjeta)
+         enlace.appendChild(tarjeta)
 
-            header[0].parentNode.insertBefore(enlace, header[0].nextSibling)
+         header[0].parentNode.insertBefore(enlace, header[0].nextSibling)
      }
 }
 
@@ -249,86 +249,91 @@ function buscar(){
    var json = jsonList()
    var header = document.getElementsByTagName("header")
      for(i; i < json.length; i++){
-        if((json[i].tipo_coche == "Compacto" || json[i].tipo_coche == "SUV") && json[i].combustible == "Gasolina"){
-            var tarjeta = document.createElement("div")
-            tarjeta.setAttribute("class", "tarjeta")
-            var espacioIm = document.createElement("div")
-            espacioIm.setAttribute("class", "espacio-imagen")
-            var imagen = document.createElement("img")
-            imagen.src = json[i].imagen
+         var tarjeta = document.createElement("div")
+         tarjeta.setAttribute("class", "tarjeta")
+         var espacioIm = document.createElement("div")
+         espacioIm.setAttribute("class", "espacio-imagen")
+         var imagen = document.createElement("img")
+         imagen.src = json[i].imagen
 
-            var contenedor = document.createElement("div")
-            contenedor.setAttribute("class", "contenedor")
-            var primeraFila = document.createElement("div")
-            primeraFila.setAttribute("class", "primera-fila")
-            var segundaFila = document.createElement("div")
-            segundaFila.setAttribute("class", "segunda-fila")
-            
-            var precioCoche = document.createElement("div")
-            precioCoche.setAttribute("class", "precio-coche")
-            var tituloPrecio = document.createElement("span")
-            tituloPrecio.textContent = "Precio: "
-            tituloPrecio.setAttribute("class", "titulo-precio")
-            var precio = document.createElement("span")
-            precio.setAttribute("class", "precio")
-            precio.textContent = json[i].precio
+         var contenedor = document.createElement("div")
+         contenedor.setAttribute("class", "contenedor")
+         var primeraFila = document.createElement("div")
+         primeraFila.setAttribute("class", "primera-fila")
+         var segundaFila = document.createElement("div")
+         segundaFila.setAttribute("class", "segunda-fila")
+         
+         var precioCoche = document.createElement("div")
+         precioCoche.setAttribute("class", "precio-coche")
+         var tituloPrecio = document.createElement("span")
+         tituloPrecio.textContent = "Precio: "
+         tituloPrecio.setAttribute("class", "titulo-precio")
+         var precio = document.createElement("span")
+         precio.setAttribute("class", "precio")
+         precio.textContent = json[i].precio
 
-            var nombre = document.createElement("span")
-            nombre.setAttribute("class", "marca")
-            nombre.textContent = json[i].marca + " " + json[i].modelo
-            var fav = document.createElement("img")
-            fav.src = "imagenes/favourites-filled-star-symbol.png"
-            fav.setAttribute("class", "favorito")
+         var nombre = document.createElement("span")
+         nombre.setAttribute("class", "marca")
+         nombre.textContent = json[i].marca + " " + json[i].modelo
+         var fav = document.createElement("img")
+         fav.src = "imagenes/favourites-filled-star-symbol.png"
+         fav.setAttribute("class", "favorito")
 
-            var anio = document.createElement("span")
-            anio.setAttribute("class", "año")
-            anio.textContent = json[i].año
-            var km = document.createElement("span")
-            km.setAttribute("class", "km")
-            km.textContent = json[i].km
-            var combustible = document.createElement("span")
-            combustible.setAttribute("class", "combustible")
-            combustible.textContent = json[i].combustible
-            var localidad = document.createElement("span")
-            localidad.setAttribute("class", "localidad")
-            localidad.textContent = json[i].localidad
+         var anio = document.createElement("span")
+         anio.setAttribute("class", "año")
+         anio.textContent = json[i].año
+         var km = document.createElement("span")
+         km.setAttribute("class", "km")
+         km.textContent = json[i].km
+         var combustible = document.createElement("span")
+         combustible.setAttribute("class", "combustible")
+         combustible.textContent = json[i].combustible
+         var localidad = document.createElement("span")
+         localidad.setAttribute("class", "localidad")
+         localidad.textContent = json[i].localidad
 
-            var datos = document.createElement("div")
-            datos.setAttribute("class", "datos-coche")
-            var aniadir = document.createElement("img")
-            aniadir.setAttribute("class", "añadir")
-            aniadir.src = "imagenes/add.png"
+         var datos = document.createElement("div")
+         datos.setAttribute("class", "datos-coche")
+         var aniadir = document.createElement("img")
+         aniadir.setAttribute("class", "añadir")
+         aniadir.src = "imagenes/add.png"
 
-            var enlace = document.createElement("a")
-            enlace.href = "descripcion_vehiculo.html"
+         var enlace = document.createElement("a")
+         enlace.href = "descripcion_vehiculo.html"
+         var enlaceFavoritos = document.createElement("a")
+         enlaceFavoritos.href = "favoritos.html"
+         var enlaceComparar = document.createElement("a")
+         enlaceComparar.href = "comparar_vehiculo.html"
 
-            espacioIm.appendChild(imagen)
+         espacioIm.appendChild(imagen)
 
-            precioCoche.appendChild(tituloPrecio)
-            precioCoche.appendChild(precio)
+         precioCoche.appendChild(tituloPrecio)
+         precioCoche.appendChild(precio)
 
-            primeraFila.appendChild(precioCoche)
-            primeraFila.appendChild(nombre)
-            primeraFila.appendChild(fav)
+         enlaceFavoritos.appendChild(fav)
+         enlaceComparar.appendChild(aniadir)
 
-            datos.appendChild(anio)
-            datos.appendChild(km)
-            datos.appendChild(combustible)
-            datos.appendChild(localidad)
+         primeraFila.appendChild(precioCoche)
+         primeraFila.appendChild(nombre)
+         primeraFila.appendChild(enlaceFavoritos)
 
-            segundaFila.appendChild(datos)
-            segundaFila.appendChild(aniadir)
+         datos.appendChild(anio)
+         datos.appendChild(km)
+         datos.appendChild(combustible)
+         datos.appendChild(localidad)
 
-            contenedor.appendChild(primeraFila)
-            contenedor.appendChild(segundaFila)
+         segundaFila.appendChild(datos)
+         segundaFila.appendChild(enlaceComparar)
 
-            tarjeta.appendChild(espacioIm)
-            tarjeta.appendChild(contenedor)
+         contenedor.appendChild(primeraFila)
+         contenedor.appendChild(segundaFila)
 
-            enlace.appendChild(tarjeta)
+         tarjeta.appendChild(espacioIm)
+         tarjeta.appendChild(contenedor)
 
-            header[0].parentNode.insertBefore(enlace, header[0].nextSibling)
-        }
+         enlace.appendChild(tarjeta)
+
+         header[0].parentNode.insertBefore(enlace, header[0].nextSibling)
      }
 }
 
@@ -361,10 +366,4 @@ function cargarVehiculo(pos){
    coche.src = json[pos].imagen
 
    document.body.appendChild(coche)
-}
-
-function nombreCoche(div){
-   for(var i = 0; i < div.childNodes.length; i++){
-      alert(div.childNodes[i].className)
-   }
 }
